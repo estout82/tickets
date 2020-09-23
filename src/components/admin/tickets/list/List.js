3385ac77a8bc8352b1a05773a8d19d65b58e4bd2
Eric Stoutenburg <eric.stoutenburg@mss-ericstoutenburg.local>
2020-09-23 16:43:55 -0700

    removed warning

--- src/components/admin/tickets/list/List.js
+++ src/components/admin/tickets/list/List.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import ListRow from './ListRow';
 import Pagenation from '../../../common/Pagenation';
