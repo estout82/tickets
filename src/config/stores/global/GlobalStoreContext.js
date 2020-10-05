c945fe18ffe6efb4aac0ecaf383beec6eda55a38
Eric Stoutenburg <eric.stoutenburg@mss-ericstoutenburg.local>
2020-10-05 14:04:06 -0700

    added neewe ticket hooks to global context

--- src/config/stores/global/GlobalStoreContext.js
+++ src/config/stores/global/GlobalStoreContext.js
@@ -4,6 +4,7 @@ import useDepartments from './useDepartments';
 import useOrganizations from './useOrganizations';
 import useMeta from '../meta/useMeta';
 import useTicketCategories from './useTicketCategories';
+import useTicketStatuses from './useTicketStatuses';
 
 export let context = createContext();
 
@@ -11,14 +12,15 @@ export function Provider({ children }) {
     const departments = useDepartments();
     const organizations = useOrganizations();
     const ticketCategories = useTicketCategories();
-    const meta = useMeta();
+    const ticketStatuses = useTicketStatuses();
 
     // function that gets a single status from the statuses of all hooks
     const getStatusText = () => {
         // if either are not done, return that status
         if (departments.status.text !== 'done') return departments.status.text;
         else if (organizations.status.text !== 'done') return organizations.status.text;
-        else if (meta.status.text !== 'done') return meta.status.text;
+        else if (ticketCategories.status.text !== 'done') return ticketCategories.status.text;
+        else if (ticketStatuses.status.text !== 'done') return ticketStatuses.status.text;
 
         // if we ar here, all statses are done
         return 'done';
@@ -29,7 +31,10 @@ export function Provider({ children }) {
         status: { text: getStatusText() },
         departments,
         organizations,
-        meta
+        ticket: {
+            statuses: ticketStatuses,
+            categories: ticketCategories
+        }
     }
 
     return (
